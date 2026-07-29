96daf891a7d7d17eb6d6836ac1567b7a2b29db37
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-07-29 20:04:27 +0200

    Incorporar novas imaxes ao arquivo da galería pública

---
+++ src/data/galeria-fotos.js
@@ -0,0 +1,268 @@
+export const fotosGaleria = [
+  {
+    id: '1aTwGKb3r0LevhWywI_A1xE9cGnZs1SYV',
+    titulo: 'Concerto en Madeira decembro de 2019',
+    categoria: 'Viaxes',
+    ano: '2019'
+  },
+  {
+    id: '11p3-YTNdNfFsKZYhHdgOMGQioLnaihIq',
+    titulo: 'Dirección da SCPP por Iglesias Vilarelle',
+    categoria: 'Histórico',
+    ano: ''
+  },
+  {
+    id: '1TEaAJ2e6jFmWvse0rIC3dOFzb5c78V6w',
+    titulo: 'Concerto Lugo 2024',
+    categoria: 'Concertos',
+    ano: '2024'
+  },
+  {
+    id: '1mSNZYp0l3xNbzNlTcEcjVmIdZEan0EJt',
+    titulo: 'Concerto no Teatro Principal con motivo das conferencias do centenario da SCPP',
+    categoria: 'Centenario',
+    ano: '2025'
+  },
+  {
+    id: '1hhPiQao1PuOHbTMTtRdUR2VFsECaMSsh',
+    titulo: 'Concerto en Afundación en beneficio do Banco de Alimentos',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '1k5o49yxkt7mT47rHzdQmnjdVtDpEkpVK',
+    titulo: 'Concerto Martín Códax 11-11-2023',
+    categoria: 'Concertos',
+    ano: '2023'
+  },
+  {
+    id: '1JJmMXkJa73ZDgK8OLsXvkITys-4rdvWK',
+    titulo: 'Concerto na Igrexa de San Francisco coa Banda de Música de Pontevedra',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '18WI_oQ5KvpoJSUq3R1AWT1npCYbiZXXu',
+    titulo: 'Concerto no Museo de Pontevedra. Medalla Marcial del Adalid',
+    categoria: 'Distincións',
+    ano: '2025'
+  },
+  {
+    id: '1D7sjPTDVo3_exlc-m7xmDeIWhpqqyfGe',
+    titulo: 'Entrega do Premio Otero Pedrayo',
+    categoria: 'Distincións',
+    ano: '2025'
+  },
+  {
+    id: '10nA7cp6Dj6KzCYXDicVWExMdkX7z6ktf',
+    titulo: 'Concerto do centenario na Igrexa de San Francisco II',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '1v7KhxHmQpjz7YeVSsd6tfmL5puPKTt8J',
+    titulo: 'Concerto do centenario na Igrexa de San Francisco',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '1yf6hbk5bptLQLfkzOpJSkrewFW_weO7j',
+    titulo: 'Entrega da Vieira de Honra á Calidade Artística',
+    categoria: 'Distincións',
+    ano: '2026'
+  },
+  {
+    id: '1PuwPc_5TqKfwNJJ6VKLLD5n_xZH3-yA8',
+    titulo: 'Prensa sobre a concesión da Medalla de Ouro «Marcial del Adalid»',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '19qIIA9ez5xOQ-1oqrCZt4XBU4ObECiaY',
+    titulo: 'Prensa do concerto de Nadal do 20 de decembro de 2025',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1sQGTB_ud0uu3YGOyyG60aaZPxRyIpW2a',
+    titulo: 'Pésame de Ramón Otero Pedrayo polo falecemento de Antonio Blanco Porto',
+    categoria: 'Histórico',
+    ano: '1940',
+    documento: true
+  },
+  {
+    id: '1QCFeksFbDIF70C8N-agothiDtSE43ews',
+    titulo: 'Pano do rosetón do Olival de Castelao',
+    categoria: 'Histórico',
+    ano: '1926'
+  },
+  {
+    id: '1Ef-zQzAr1dacYaEnb6ry_-b0VTU3WeQW',
+    titulo: 'Pilar Baamonde Lusquiños co selo conmemorativo do centenario',
+    categoria: 'Centenario',
+    ano: '2025'
+  },
+  {
+    id: '1zjCekKz_x7ApVh-8MoigqaiGGSCWSTef',
+    titulo: 'Loa de Celso Emilio Ferreiro a Iglesias Vilarelle',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1du5niKrQ5Elr9Cwa2GfGfeuRMJygC4rr',
+    titulo: 'Entrega da Medalla Marcial del Adalid no Museo de Pontevedra',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1h2MlvIMd3qWMNlXAMiKwbx8Y1aRXepzP',
+    titulo: 'Inauguración da exposición do centenario da Polifónica',
+    categoria: 'Centenario',
+    ano: '2025'
+  },
+  {
+    id: '1N7npt12gy6f5rpOF6T44sCUBq3aVe1t7',
+    titulo: 'Inauguración da exposición do centenario no Museo de Pontevedra',
+    categoria: 'Centenario',
+    ano: '2025'
+  },
+  {
+    id: '1aHNBvqewXVxHhwtJhuNibJiAq-6GKh7J',
+    titulo: 'A Coral diante da fachada de Santa María con Antonio Blanco Porto',
+    categoria: 'Histórico',
+    ano: '1925'
+  },
+  {
+    id: '1o77pOMMlA6m6OFA22gPGyn8O9rhZ-9pe',
+    titulo: 'Faro de Vigo sobre o Premio Otero Pedrayo',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1d7Ap-1lqqJSOzZC58x0WOXGMOaaN_dQi',
+    titulo: 'Faro de Vigo sobre a entrega do Premio Otero Pedrayo',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1nT4Zt-FSACtq-DaSHfNNUeTwLrJiOQ6Y',
+    titulo: 'A Sociedade Coral dirixida por Bertomeu en 1975',
+    categoria: 'Histórico',
+    ano: '1975'
+  },
+  {
+    id: '1l028OraZNNVcvNkHgmu46iGbO9OS8tlt',
+    titulo: 'Diario de Pontevedra: inauguración da exposición do centenario',
+    categoria: 'Prensa',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1PR814KdX_puh4klog1OtzhA6aAiXhfGm',
+    titulo: 'Primeiro concerto da SCPP no Teatro Arriaga de Bilbao',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1dsTjC7d16TQ0KdWiLW9q54IoI4UV2l0h',
+    titulo: 'Crónica da Polifónica de 1944',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1SIm6O0Q8Zp3bEkZAAy3DoI8a5lDlOeWz',
+    titulo: 'Concerto de despedida do Nadal de xaneiro de 2025',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '1n5vTikmVq2z9ldvKAd7n_Q5IikxhwJm4',
+    titulo: 'Crónica do segundo concerto da SCPP no Teatro Arriaga',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1gxx4dHIWyYBrLkEI7SCOxIjHn3nBU1wP',
+    titulo: 'Concerto polo 30 aniversario do Banco de Alimentos',
+    categoria: 'Concertos',
+    ano: '2025'
+  },
+  {
+    id: '1tIsmGEyjzk5tyLwYkCkKkctrRFacJOaG',
+    titulo: 'Concerto no Teatro García Barbón de Vigo',
+    categoria: 'Histórico',
+    ano: '1936',
+    documento: true
+  },
+  {
+    id: '1GoWynZt4kc5c8hIP5KLofFdUHPbBByHl',
+    titulo: 'Crónica do concerto no Círculo de Belas Artes de Madrid',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1qJE1g-pZOqBUlMXBTi20M7WQOIYmvj07',
+    titulo: 'Notas ao programa do concerto no Teatro Arriaga',
+    categoria: 'Histórico',
+    ano: '1944',
+    documento: true
+  },
+  {
+    id: '1pPUKlns_Ef_cjvvHuPCs5aVO7flz5YM4',
+    titulo: 'Cartel da exposición do centenario no Museo de Pontevedra',
+    categoria: 'Centenario',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1v8NWO8cQZ4um5RPZa0PkTq0qsckOwEBV',
+    titulo: 'Cartel do concerto homenaxe da Sociedade Filharmónica',
+    categoria: 'Concertos',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1Msex4oJeiffwFihd4OyRn0LHMjDTMUky',
+    titulo: 'Programa de man do concerto homenaxe da Sociedade Filharmónica',
+    categoria: 'Concertos',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1ql3vEB9B0beKLIXwrxY4gKaMX8znwTfR',
+    titulo: 'Cartel das xornadas de conferencias do centenario',
+    categoria: 'Centenario',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1VWBqnaJ0X8UInCnN4i8JhuuTsEhIObdD',
+    titulo: 'Cartel da entrega da Medalla Marcial del Adalid',
+    categoria: 'Distincións',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1kHe2u8BRi4MYHeq5NOMPqceC951I6-jE',
+    titulo: 'Cartel do concerto de Nadal en San Francisco',
+    categoria: 'Concertos',
+    ano: '2025',
+    documento: true
+  },
+  {
+    id: '1u1QkvA6RqZFsqEJ1va3H1pyfOjbD9eHw',
+    titulo: 'Imaxe promocional do concerto de San Francisco',
+    categoria: 'Concertos',
+    ano: '2025'
+  }
+];
