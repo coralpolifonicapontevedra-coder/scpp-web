2f91373ef2e05a2790c20d1b7eee127b6468e3d5
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-08-03 12:47:48 +0200

    Neutralizar recuperación duplicada da galería

--- functions/galeria/_middleware.js
+++ functions/galeria/_middleware.js
@@ -1,21 +1,3 @@
 export async function onRequest(context) {
-  const response = await context.next();
-  const contentType = response.headers.get('Content-Type') || '';
-  if (!contentType.includes('text/html')) return response;
-
-  const html = await response.text();
-  const script = '<script src="/js/galeria-recovery.js?v=20260803-1" defer></script>';
-  const body = html.includes('</body>')
-    ? html.replace('</body>', `${script}</body>`)
-    : `${html}${script}`;
-
-  const headers = new Headers(response.headers);
-  headers.delete('Content-Length');
-  headers.set('Cache-Control', 'no-store');
-
-  return new Response(body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers
-  });
+  return context.next();
 }
