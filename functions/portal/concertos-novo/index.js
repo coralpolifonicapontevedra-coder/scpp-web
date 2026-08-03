8f02ff0ed5cef813987124a5243ffd236b7dc095
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-08-03 14:05:18 +0200

    Actualizar versión visual de concertos novo

--- functions/portal/concertos-novo/index.js
+++ functions/portal/concertos-novo/index.js
@@ -9,8 +9,8 @@ export async function onRequestGet({ request, env }) {
   let html = await resposta.text();
 
   const recursos = [
-    '<link rel="stylesheet" href="/css/concertos-novo-visual.css?v=1">',
-    '<script type="module" src="/js/concertos-novo-visual.js?v=1"></script>'
+    '<link rel="stylesheet" href="/css/concertos-novo-visual.css?v=2">',
+    '<script type="module" src="/js/concertos-novo-visual.js?v=2"></script>'
   ];
 
   recursos.forEach((recurso) => {
