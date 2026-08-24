c8c467b5083f261f61fd0217e97104aa470a58d1
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-08-24 21:49:20 +0200

    fix(preview): conectar enlaces do programa ao repertorio

--- functions/portal/concertos/index.js
+++ functions/portal/concertos/index.js
@@ -50,6 +50,7 @@ export async function onRequestGet({ request, env }) {
     '<link rel="stylesheet" href="/css/concertos-novo-informe.css?v=1">',
     '<link rel="stylesheet" href="/css/concertos-seccions.css?v=1">',
     '<script type="module" src="/js/concertos-novo-clasico.js?v=2"></script>',
+    '<script type="module" src="/js/concertos-program-links.js?v=1"></script>',
     '<script type="module" src="/js/concertos-cartel-r2.js?v=1"></script>',
     '<script type="module" src="/js/concertos-seccions.js?v=1"></script>'
   ];
@@ -75,7 +76,7 @@ export async function onRequestGet({ request, env }) {
   cabeceiras.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
   cabeceiras.set('Pragma', 'no-cache');
   cabeceiras.set('Expires', '0');
-  cabeceiras.set('X-SCPP-Concertos-Version', 'oficial-v3');
+  cabeceiras.set('X-SCPP-Concertos-Version', 'oficial-v4');
 
   return new Response(html, {
     status: resposta.status,
