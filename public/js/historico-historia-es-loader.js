0725bd35caa979d4fd941b56def7a528617c286c
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-08-28 13:03:29 +0200

    Retirar cargador auxiliar no conectado

--- public/js/historico-historia-es-loader.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  if (!document.querySelector('#panel-historico')) return;
-  const script = document.createElement('script');
-  script.src = '/js/historico-historia-es.js?v=20260828-3';
-  script.async = false;
-  document.body.appendChild(script);
-})();
