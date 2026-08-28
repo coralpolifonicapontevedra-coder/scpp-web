09844fc41f263581835ce7a908642ad41ed8f836
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-08-28 19:07:11 +0200

    actualidad: mostrar enlaces a los dos periódicos

--- functions/es/actualidad/_middleware.js
+++ functions/es/actualidad/_middleware.js
@@ -1,4 +1,5 @@
 const ORIXE = 'https://script.google.com/macros/s/AKfycbyFrlkJW9Ur1gRVRtIXOucfdr7zFzVGiL_V3KCHbot8IkNvoAXylP7-Dta2X-ki7bEh/exec?recurso=publicacions';
+const PRENSA_SCRIPT = '<script src="/js/actualidade-prensa-20260828.js?v=20260828-1"></script>';
 
 export async function onRequest(context) {
   const response = await context.next();
@@ -9,6 +10,10 @@ export async function onRequest(context) {
   html = html
     .replace(ORIXE, '/api/actualidade')
     .replace("cache: 'no-store'", "cache: 'default'");
+
+  if (!html.includes('/js/actualidade-prensa-20260828.js')) {
+    html = html.replace('</body>', `${PRENSA_SCRIPT}</body>`);
+  }
 
   const headers = new Headers(response.headers);
   headers.delete('Content-Length');
