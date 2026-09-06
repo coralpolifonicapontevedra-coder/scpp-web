eaba363c8d83374865dbc18cd986fe433154d0b2
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-09-06 22:13:24 +0200

    chore: bust repertoire loader cache after R2 sync fix

--- functions/portal/repertorio/_middleware.js
+++ functions/portal/repertorio/_middleware.js
@@ -5,8 +5,8 @@ export async function onRequest(context) {
 
   const html = await response.text();
   const scripts = [
-    '<script src="/js/repertorio-direct-api.js?v=20260803-2"></script>',
-    '<script src="/js/repertorio-r2-bridge.js?v=20260806-grid-2"></script>'
+    '<script src="/js/repertorio-direct-api.js?v=20260906-r2sync-1"></script>',
+    '<script src="/js/repertorio-r2-bridge.js?v=20260906-r2sync-1"></script>'
   ].join('');
   const body = html.includes('</body>')
     ? html.replace('</body>', `${scripts}</body>`)
