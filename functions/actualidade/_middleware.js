4577549f82324df893edf28ce2853866ceee1516
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-09-11 13:36:12 +0200

    Cargar bloque de prensa Marcial del Adalid

--- functions/actualidade/_middleware.js
+++ functions/actualidade/_middleware.js
@@ -1,5 +1,6 @@
 const ORIXE = 'https://script.google.com/macros/s/AKfycbyFrlkJW9Ur1gRVRtIXOucfdr7zFzVGiL_V3KCHbot8IkNvoAXylP7-Dta2X-ki7bEh/exec?recurso=publicacions';
 const PRENSA_SCRIPT = '<script src="/js/actualidade-prensa-20260828.js?v=20260901-1"></script>';
+const ADALID_SCRIPT = '<script src="/js/actualidade-adalid-20260910.js?v=20260911-1"></script>';
 
 export async function onRequest(context) {
   const response = await context.next();
@@ -11,9 +12,8 @@ export async function onRequest(context) {
     .replace(ORIXE, '/api/actualidade')
     .replace("cache: 'no-store'", "cache: 'default'");
 
-  if (!html.includes('/js/actualidade-prensa-20260828.js')) {
-    html = html.replace('</body>', `${PRENSA_SCRIPT}</body>`);
-  }
+  if (!html.includes('/js/actualidade-prensa-20260828.js')) html = html.replace('</body>', `${PRENSA_SCRIPT}</body>`);
+  if (!html.includes('/js/actualidade-adalid-20260910.js')) html = html.replace('</body>', `${ADALID_SCRIPT}</body>`);
 
   const headers = new Headers(response.headers);
   headers.delete('Content-Length');
@@ -21,9 +21,5 @@ export async function onRequest(context) {
   headers.delete('ETag');
   headers.set('Cache-Control', 'public, max-age=60, s-maxage=300, stale-while-revalidate=3600');
 
-  return new Response(html, {
-    status: response.status,
-    statusText: response.statusText,
-    headers
-  });
+  return new Response(html, { status: response.status, statusText: response.statusText, headers });
 }
