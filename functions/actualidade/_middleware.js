088ebf34cab8514256d75be47d2eaa8c5f4a53d8
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-09-11 18:07:30 +0200

    Actualidade: aplicar corrección dos enlaces de Adalid

--- functions/actualidade/_middleware.js
+++ functions/actualidade/_middleware.js
@@ -1,6 +1,7 @@
 const ORIXE = 'https://script.google.com/macros/s/AKfycbyFrlkJW9Ur1gRVRtIXOucfdr7zFzVGiL_V3KCHbot8IkNvoAXylP7-Dta2X-ki7bEh/exec?recurso=publicacions';
 const PRENSA_SCRIPT = '<script src="/js/actualidade-prensa-20260828.js?v=20260901-1"></script>';
 const ADALID_SCRIPT = '<script src="/js/actualidade-adalid-20260910.js?v=20260911-2"></script>';
+const ADALID_LINKS_FIX_SCRIPT = '<script src="/js/actualidade-adalid-links-fix-20260911.js?v=20260911-1"></script>';
 
 export async function onRequest(context) {
   const response = await context.next();
@@ -14,6 +15,7 @@ export async function onRequest(context) {
 
   if (!html.includes('/js/actualidade-prensa-20260828.js')) html = html.replace('</body>', `${PRENSA_SCRIPT}</body>`);
   if (!html.includes('/js/actualidade-adalid-20260910.js')) html = html.replace('</body>', `${ADALID_SCRIPT}</body>`);
+  if (!html.includes('/js/actualidade-adalid-links-fix-20260911.js')) html = html.replace('</body>', `${ADALID_LINKS_FIX_SCRIPT}</body>`);
 
   const headers = new Headers(response.headers);
   headers.delete('Content-Length');
