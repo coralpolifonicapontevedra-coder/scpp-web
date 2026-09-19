66a73504c43259cc95e8e23e650b5ad64b61ec21
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-09-19 13:57:13 +0200

    fix(migracion): usar carpeta corporativa de fotografías

--- apps-script-production/permisos-fotos-drive.js
+++ apps-script-production/permisos-fotos-drive.js
@@ -7,7 +7,7 @@
 
 function facerPublicasFotosDrive() {
   const folderId =
-    '1FySxDvTHVNC20-a3I0wDU1v0s82VRiix';
+    '1iXjZPmg330JEpuBXrioeJmAAQXUzNlFW';
 
   const carpeta =
     DriveApp.getFolderById(folderId);
