51cb475ffad28830741eb11a6b34cc018f788b50
coralpolifonicapontevedra-coder <coralpolifonicapontevedra@gmail.com>
2026-09-19 10:47:42 +0200

    fix(migracion): apuntar automatizacións ás Sheets corporativas

--- scripts/import-presidencias-fotos-r2.py
+++ scripts/import-presidencias-fotos-r2.py
@@ -20,7 +20,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 from PIL import Image, ImageOps
 
-SPREADSHEET_ID = "1NhWEnrlOk285ECxUQMB3Pedd28TNkiMmN-K25vzd_2w"
+SPREADSHEET_ID = "1KuSQDBk1-7WfDtO7nxKGQGTVggpwdI3kRzPSuKlMQrg"
 SHEET_RANGE = "Fotos!A1:AB5000"
 FOLDER_ID = "1FySxDvTHVNC20-a3I0wDU1v0s82VRiix"
 BUCKET = "scpp-publico"
